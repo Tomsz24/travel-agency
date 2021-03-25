fb792602fb5261bb60a9a3bef40e682307c5da62
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2021-03-25 22:23:19 +0100

    extended routing and add animations to it

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -1,12 +1,12 @@
 /* SELECTORS */
 
-export const getAllTrips = ({trips}) => trips;
+export const getAllTrips = ({ trips }) => trips;
 
-export const getFilteredTrips = ({trips, filters}) => {
+export const getFilteredTrips = ({ trips, filters }) => {
   let output = trips;
 
   // filter by search phrase
-  if(filters.searchPhrase){
+  if (filters.searchPhrase) {
     const pattern = new RegExp(filters.searchPhrase, 'i');
     output = output.filter(trip => pattern.test(trip.name));
   }
@@ -20,22 +20,24 @@ export const getFilteredTrips = ({trips, filters}) => {
   return output;
 };
 
-export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
+export const getTripById = ({ trips }, tripId) => {
+  const filtered = [];
 
   // TODO - filter trips by tripId
+  trips.forEach(trip => { trip.id === tripId ? filtered.push(trip) : ''; });
 
   console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+  return filtered.length ? filtered[0] : { error: true };
 };
 
-export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
+export const getTripsForCountry = ({ trips }, countryCode) => {
+  const filtered = [];
 
   // TODO - filter trips by countryCode
 
+  trips.forEach(trip => { trip.country.code === countryCode ? filtered.push(trip) : ''; });
   console.log('filtering trips by countryCode:', countryCode, filtered);
-  return filtered.length ? filtered : [{error: true}];
+  return filtered.length ? filtered : [{ error: true }];
 };
 
 /* ACTIONS */
