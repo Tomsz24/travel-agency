ab7ed7ba0507576342c144e0f543b8b5f91aa1e4
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2021-04-06 16:29:34 +0100

    moved all required props to order components

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 
 const OrderForm = props => {
   const { tripCost, options } = props;
   return (
     <Row>
+      {pricing.map(item => (
+        <Col md={4} key={item.id}>
+          <OrderOption name={item.name} type={item.type} options={item} currentValue={options[item.id]} />
+        </Col>
+      ))}
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} options={options} />
       </Col>
@@ -16,7 +23,7 @@ const OrderForm = props => {
 };
 
 OrderForm.propTypes = {
-  tripCost: PropTypes.any,
+  tripCost: PropTypes.string,
   options: PropTypes.object,
 };
 
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
+import OrderOptionNumber from './OrderOptionNumber';
+import OrderOptionIcons from './OrderOptionIcons';
+import OrderOptionDropdown from './OrderOptionDropdown';
+
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
+
+// const OrderOption = props => {
+//   const { component, title } = styles;
+//   const { name } = props;
+//   return (
+//     <div className={component}>
+//       <h3 className={title}>{name}</h3>
+//     </div>
+//   );
+// }
+
+const OrderOption = ({ name, type, ...otherProps }) => {
+  const OptionComponent = optionTypes[type];
+  if (!OptionComponent) {
+    return null;
+  } else {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{name}</h3>
+        <OptionComponent
+          {...otherProps}
+        />
+      </div>
+    );
+  }
+};
+
+export default OrderOption;
