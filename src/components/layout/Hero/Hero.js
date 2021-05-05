b6b952c94bbbadc9aa94ad33bb38375dec087ffa
Tomasz <tomaszwojciechowski244@gmail>
2021-05-05 22:49:19 +0200

    new component, tests, functionality and implementation added with TDD

--- src/components/layout/Hero/Hero.js
+++ src/components/layout/Hero/Hero.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './Hero.scss';
+import HappyHoursAd from '../../features/HappyHourAd/HappyHourAd';
 import PropTypes from 'prop-types';
 
 const Hero = ({ variant = '', titleText, imageSrc, ...otherProps }) => (
   <div {...otherProps} className={styles.component + variant.split(' ').map(name => ' ' + (styles[name] || name)).join('')}>
     <h2 className={styles.title}>{titleText}</h2>
     <img className={styles.image} src={imageSrc} />
+    <div className={styles.happyHour}>
+      <HappyHoursAd />
+    </div>
   </div>
 );
 
