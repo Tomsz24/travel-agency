072ddadbb3f50d14af6262c6b81e221a7da5a0f4
Tomasz <tomaszwojciechowski244@gmail>
2021-06-02 16:08:58 +0200

    create component and test for DaysForSummer

--- src/components/layout/Hero/Hero.js
+++ src/components/layout/Hero/Hero.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Hero.scss';
+import DaysToSummer from '../../features/DaysToSummer/DaysToSummer';
 import HappyHoursAd from '../../features/HappyHourAd/HappyHourAd';
 import PropTypes from 'prop-types';
 
 const Hero = ({ variant = '', titleText, imageSrc, ...otherProps }) => (
   <div {...otherProps} className={styles.component + variant.split(' ').map(name => ' ' + (styles[name] || name)).join('')}>
+    <div className={styles.daysToSummer}>
+      <DaysToSummer />
+    </div>
     <h2 className={styles.title}>{titleText}</h2>
     <img className={styles.image} src={imageSrc} />
     <div className={styles.happyHour}>
